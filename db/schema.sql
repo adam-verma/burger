5634b01499a745fdf6d843a72913997963698a04
Adam Verma <vermaadam@gmail.com>
2020-01-06 15:45:10 -0600

    schema.sql database and table created

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- Drop the burgers_db database if already exists 
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Create database and specify it for use
+CREATE DATABASE burgers_db; 
+
+USE burgers_db;
+
+-- Create the table burger.
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT,
+  burger_name varchar(255) NOT NULL,
+  devoured boolean NOT NULL,
+  PRIMARY KEY (id)
+);
