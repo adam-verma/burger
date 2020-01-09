38e26c9b1929cd2b94675f8f39e3513ad0b6017f
Adam Verma <vermaadam@gmail.com>
2020-01-09 01:51:46 -0600

    orm.js created helper objToSql for converting objects to SQL syntax

--- config/orm.js
+++ config/orm.js
@@ -1,14 +1,36 @@
 // Require connection.js
 const connection = require('./connection.js');
 
-// Write Helpers for: creating SQL value placeholders as 'string' type AND converting key/value pairs in objects to proper SQL syntax.
+// Write Helper for: creating SQL value placeholders as 'string' type
 function questMarktoStr(num) {
-    placeholder = [];
+    const placeholder = [];
 
     for (let i=0; i<num; i++) {
         placeholder.push("?"); 
     }
     return placeholder.toString();
+}
+
+// Write Helper for: converting key/value pairs in objects to proper SQL syntax.
+function objToSql(obj) {
+    const arr = []; 
+   
+   
+    for (let key in obj) {
+        let value = obj[key]; 
+
+         // Check if key/value pair is a specific property in object
+    if (Object.hasOwnProperty.call(obj, key)) {
+        // If string with spaces, add quotations to enclose
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = `'${value}'`
+        }
+
+        arr.push(`${key} = ${value}`);
+    }
+    // Tranlate array of strings to a single comma-separated string
+    return arr.toString(); 
+}
 }
 
 // MySQL commands for manipulating data
@@ -35,7 +57,7 @@ const orm = {
     },
 
     updateOne: function(table_name, col_name, condition, cb) {
-        const queryStr = `UPDATE ${table_name} SET ${col_name} WHERE ${condition}`
+        const queryStr = `UPDATE ${table_name} SET ${objToSql(col_name)} WHERE ${condition}`
         connection.query(queryStr, function(err,res) {
             if (err) {
                 throw err;
