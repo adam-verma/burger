eeabe795393594987827affafdcd6174540ec5eb
Adam Verma <vermaadam@gmail.com>
2020-01-09 01:36:21 -0600

    burger.js created model functions in burger object for database callbacks

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,24 @@
 // Require orm.js to create functions that interact with data from database
 const orm = require('../config/orm.js');
+
+burger = { 
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res)
+        });   
+    },
+    
+    insertOne: function(col_name, vals, cb) {
+        orm.insertOne("burgers", col_name, vals, function(res) {
+            cb(res); 
+        });
+    },
+
+    updateOne: function(col_name, condition, cb) {
+        orm.updateOne("burgers", col_name, condition, function(res) {
+            cb(res); 
+        });    
+    },
+ }
+
+ module.exports = burger; 
